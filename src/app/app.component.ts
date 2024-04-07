90f70cf0f6fdf108324a8f3f2cb4d578c93c7e8c
ВЛАДИСЛАВ ВЛАДИСЛАВ <Vladislav290403@gmail.com>
2024-04-07 13:31:11 +0400

    Complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'laba2';
+  greeting: string = "";
+
+  ngOnInit(): void {
+    console.log('Я родился!');
+  }
+  
+  ngOnDestroy(): void {
+    console.log('ngOnDestroy');
+  }
+
+  ngOnChanges(): void {
+    console.log('ngOnChanges');
+  }
+
+  ngDoCheck(): void {
+    console.log('ngDoCheck');
+  }
+
+  ngAfterContentInit(): void {
+    console.log('ngAfterContentInit');
+  }
+
+  ngAfterContentChecked(): void {
+    console.log('ngAfterContentChecked');
+  }
+
+  ngAfterViewInit(): void {
+    console.log('ngAfterViewInit');
+  }
+
+  ngAfterViewChecked(): void {
+    console.log('ngAfterViewChecked');
+  }
+
+  onSayHi(): void {
+    alert("Привеват, " + this.greeting);
+  }
+
 }
